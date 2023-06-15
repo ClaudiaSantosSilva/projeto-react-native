acb1e06df5edf9dfda0760f66835a782fd619488
claudia.tss <claudia.tss@outlook.com>
2023-06-15 17:34:43 -0300

    Setup inicial finalizado

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.js to start your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -17,4 +17,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  text:{
+    color:"green"
+  }
 });
+
+
+{/*<View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text style={styles.text}>Open up App.js to start your app!</Text>
+</View>*/}
+
+//<StatusBar style="auto" />
