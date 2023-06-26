5e0a19808ab0e57a82cf334f89578eb9c9b2afb8
claudia.tss <claudia.tss@outlook.com>
2023-06-25 21:30:20 -0300

    CreatePostScreen parcial

--- src/screens/CreatePostScreen.js
+++ src/screens/CreatePostScreen.js
@@ -1,9 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Pressable } from "react-native";
+import { Title } from "../components/Title";
+import styled from "styled-components/native";
+
+const Container= styled.View`
+margin: 14px;
+gap: 16px;
+`;
+
+const Button = styled.TouchableOpacity`
+  background-color: #0984e3;
+  border-radius: 8px;
+  padding: 8px;
+`;
+
+const TextButton= styled.Text`
+color: white;
+text-align: center;
+`;
 
 export function CreatePostScreen() {
   return( 
-    <View>
-      <Text>CreatePostScreen</Text>
-    </View>
+    <Container>
+      <TextInput placeholder="Digite o título" />
+      <TextInput placeholder= "Digite o subtitulo" />
+      <TextInput placeholder = "Digite o conteúdo" />
+      <Button>
+        <TextButton>Enviar</TextButton>
+      </Button>
+    </Container>
   );
 }
