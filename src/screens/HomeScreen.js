8ec926933fc64671ede3792de0e3bd941b7ab4c0
claudia.tss <claudia.tss@outlook.com>
2023-06-28 17:57:10 -0300

    Carregamento geolocalização do dispositivo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,20 +4,29 @@ import MapView, {PROVIDER_GOOGLE, MapMarker } from "react-native-maps"
 import * as Location from "expo-location"
 import Toast from "react-native-root-toast"
 import screens from "../screens.json"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { axiosApi } from "../axiosApi";
 
-const initialPostsList = {
-  count: 0,
-  notepads: [],
+const coordsDelta= 0.05;
+
+const initialCoords = {
+  latitude: 0,
+  longitude: 0,
 };
 
 
 export function HomeScreen({navigation}) {
+const [coords, setCoords] = useState(initialCoords);
+const region = {
+  ...coords,
+  latitudeDelta: coordsDelta,
+  longitudeDelta: coordsDelta,
+};
 
   async function loadGeolocation(){
-  const response = await Location.requestForegroundPermissionsAsync();
-  const position= await Location.getCurrentPositionAsync
+    const response = await Location.requestForegroundPermissionsAsync();
+    const position= await Location.getCurrentPositionAsync ();
+    setCoords(position.coords)
      
   }
 
@@ -25,16 +34,27 @@ export function HomeScreen({navigation}) {
 useEffect(()=> {
   loadGeolocation();
   
-});
+}, []);
 
   return (
     <View style={{ width: "100%", height: "100%" }}>
       <MapView
+        region= {region}
         showsUserLocation
         style={{ width: "100%", height: "80%" }}
         provider={PROVIDER_GOOGLE}
       />
-      <Text style={{fontSize: 18, marginTop:16, marginHorizontal:10, textAlign:"center"}}>Escolha o ponto no mapa e conheça mais um pouco sobre esse lugar ou selecione um ponto e adicione seus comentários. </Text>
+      <Text
+        style={{
+          fontSize: 18,
+          marginTop: 16,
+          marginHorizontal: 10,
+          textAlign: "center",
+        }}
+      >
+        Escolha uma marcação no mapa e conheça mais um pouco sobre esse lugar ou
+        selecione um ponto e adicione seus comentários.{" "}
+      </Text>
     </View>
   );
 }
