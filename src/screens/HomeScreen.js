979e07dbbe7724db022c66c3bdcf9b567c50af6e
claudia.tss <claudia.tss@outlook.com>
2023-06-28 21:01:57 -0300

    Implementação postsMarkers

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -14,46 +14,87 @@ const initialCoords = {
   longitude: 0,
 };
 
+const initialPostsList={
+  count: 0,
+  notepads: [],
+};
+
 
 export function HomeScreen({navigation}) {
 const [coords, setCoords] = useState(initialCoords);
+const [{ count, notepads }, setPostsList] = useState(initialPostsList);
+
 const region = {
   ...coords,
   latitudeDelta: coordsDelta,
   longitudeDelta: coordsDelta,
 };
 
+
+const postsInMap = notepads.filter(
+  (notepad) => notepad.latitude !== undefined && notepad.longitude !== undefined
+);
+
+async function loadPosts() {
+  const response = await axiosApi.get("/notepads", {
+    params: {
+      limit: Infinity
+    }
+  });
+  setPostsList(response.data);
+}
+
   async function loadGeolocation(){
     const response = await Location.requestForegroundPermissionsAsync();
     const position= await Location.getCurrentPositionAsync ();
     setCoords(position.coords)
-     
+  }
+
+  function onMarkerPress(notepad) {
+    navigation.navigate(screens.viewPost, {id:notepad.id});
   }
 
 
 useEffect(()=> {
   loadGeolocation();
+  const unsubscribe = navigation.addListener("focus", () => {
+    loadPosts();
+  });
+
+  return unsubscribe;
   
 }, []);
+
+const postsMarkers = postsInMap.map((notepad)=> (
+  <MapMarker key={notepad.id} coordinate={notepad} 
+  onPress={()=> onMarkerPress (notepad)} />
+))
 
   return (
     <View style={{ width: "100%", height: "100%" }}>
       <MapView
-        region= {region}
+        region={region}
         showsUserLocation
         style={{ width: "100%", height: "80%" }}
         provider={PROVIDER_GOOGLE}
-      />
+        onLongPress={(event)=>{
+          const coords = event.nativeEvent.coordinate;
+          navigation.navigate(screens.createPost, { coords });
+        }}
+      >
+        {postsMarkers}
+      </MapView>
       <Text
         style={{
           fontSize: 18,
+          color: "#3c40c6",
           marginTop: 16,
           marginHorizontal: 10,
           textAlign: "center",
         }}
       >
-        Escolha uma marcação no mapa e conheça mais um pouco sobre esse lugar ou
-        selecione um ponto e adicione seus comentários.{" "}
+        Escolha uma marcação no mapa e leia sobre esse lugar ou selecione um
+        ponto e adicione seus comentários.
       </Text>
     </View>
   );
